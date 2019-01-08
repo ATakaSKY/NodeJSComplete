161a2dd0d867b3496467d9191c7770034d68fc6b
Thakur <aakthakur@deloitte.com>
2019-01-08 14:08:21 +0530

    added app witout database

---
+++ controllers/admin.js
@@ -0,0 +1,72 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  if (!JSON.parse(editMode)) {
+    return res.redirect('/');
+  }
+
+  Product.fetchById(req.params.productId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  
+  const product = new Product(prodId, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/admin/products');
+};
+
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.deleteProduct(prodId, deleteStatus => {
+    if (deleteStatus) {
+      return res.redirect('/admin/products');
+    }
+    res.redirect('/');
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
---
+++ models/product.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+
+const Cart = require('./cart');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      if (this.id) {
+        const productIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[productIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static deleteProduct(id, cb) {
+    getProductsFromFile(products => {
+      const productToDelete = products.find(p => p.id === id);
+      const productIndex = products.findIndex(p => p.id === id);
+      const updatedProducts = [...products];
+      if (productIndex != -1) {
+        updatedProducts.splice(productIndex, 1);
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          if (!err) {
+            Cart.deleteProductFromCart(id, productToDelete.price);
+            return cb(true);
+          }
+        });
+      } else {
+        return cb(false);
+      }
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static fetchById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      return cb(product);
+    });
+  }
+};
